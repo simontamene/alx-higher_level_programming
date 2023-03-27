5cf43d5509d11febf56e0fffeef7d1d9255dcdd7
simontamene <simalike245@gmail.com>
2023-03-27 07:08:18 -0700

    ooo

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-
-
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except:
+    except BaseException:
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except BaseException:
         return False
